f7b94aedc0df45418939e7419ea7d0f11eaf2dc8
Dian Todorov <dido@uber.com>
2022-07-20 14:50:31 +0300

    Setup local Development
    Summary: Modify docker-compose.yml and server.py
    - add phpmyadmin
    - remove running server from docker
    - fix ports for db

--- src/server.py
+++ src/server.py
@@ -5,58 +5,68 @@ from dataclasses import dataclass
 
 server = Flask(__name__)
 
-server.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(
+server.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(
     os.getenv('DB_USER', 'admin'),
     os.getenv('DB_PASSWORD', 'mysql'),
-    os.getenv('DB_HOST', 'mysql'),
+    os.getenv('DB_HOST', 'localhost'),
+    os.getenv('DB_PORT', '3307'),
     os.getenv('DB_NAME', 'db')
 )
+server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+print(server.config['SQLALCHEMY_DATABASE_URI'])
 db = SQLAlchemy(server)
+
 
 @dataclass
 class Log(db.Model):
-  id: int
-  log: str
+    id: int
+    log: str
 
-  id = db.Column(db.Integer, primary_key=True)
-  log = db.Column(db.String(256), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    log = db.Column(db.String(256), nullable=False)
 
-  def __repr__(self):
-      return '<Log %r>' % self.log
+    def __repr__(self):
+        return '<Log %r>' % self.log
 
-  def __init__(self, message):
-    self.log = message
+    def __init__(self, message):
+        self.log = message
+
 
 # create the DB on demand
 @server.before_first_request
 def create_tables():
     db.create_all()
 
+
 @server.route("/")
 def hello():
-  return "Hello World!"
+    return "Hello World!"
+
 
 @server.route("/hello")
 def personalised_hello():
-  username = request.args.get('user')
-  return f'Hello {username}!'
+    username = request.args.get('user')
+    return f'Hello {username}!'
 
-@server.route('/logs', methods = ['POST'])
+
+@server.route('/logs', methods=['POST'])
 def insert_log():
-  request_data = request.get_json()
+    request_data = request.get_json()
 
-  log_message = request_data['log_message']
-  log = Log(log_message)
+    log_message = request_data['log_message']
+    log = Log(log_message)
 
-  db.session.add(log)
-  db.session.commit()
-  return 'success!'
+    db.session.add(log)
+    db.session.commit()
+    return 'success!'
 
-@server.route('/logs', methods = ['GET'])
+
+@server.route('/logs', methods=['GET'])
 def get_all_logs():
-  logs = Log.query.all()
-  return jsonify(logs)
+    logs = Log.query.all()
+    return jsonify(logs)
+
 
 if __name__ == "__main__":
-   server_port = os.environ['FLASK_PORT']
-   server.run(host='0.0.0.0', port=server_port, debug=True)
+    server_port = os.environ['FLASK_PORT']
+    server.run(host='0.0.0.0', port=server_port, debug=True)
